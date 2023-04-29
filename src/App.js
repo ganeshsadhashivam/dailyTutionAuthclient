850063848b17a52aecf16a72cf5009eedfb4f333
ganesh <ganeshsadhashivam@outlook.com>
2023-04-29 18:47:49 +0530

    login page

--- src/App.js
+++ src/App.js
@@ -1,24 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import "./App.css";
+
+/*import all components */
+import Username from "./components/Username";
+import Password from "./components/Password";
+import Register from "./components/Register";
+import Profile from "./components/Profile";
+import Recovery from "./components/Recovery";
+import Reset from "./components/Reset";
+import PageNotfound from "./components/PageNotfound";
+
+/*route route */
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Username>Root Route</Username>,
+  },
+  {
+    path: "/register",
+    element: <Register>Register Route</Register>,
+  },
+  {
+    path: "/password",
+    element: <Password>Root Route</Password>,
+  },
+  {
+    path: "/profile",
+    element: <Profile>Register Route</Profile>,
+  },
+  {
+    path: "/recovery",
+    element: <Recovery>Root Route</Recovery>,
+  },
+  {
+    path: "/reset",
+    element: <Reset>Register Route</Reset>,
+  },
+  {
+    path: "/*",
+    element: <PageNotfound>Root Route</PageNotfound>,
+  },
+]);
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main className="App">
+      <RouterProvider router={router}></RouterProvider>
+    </main>
   );
 }
 

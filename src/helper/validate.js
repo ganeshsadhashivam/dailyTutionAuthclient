1d210b279bfb511819142f8b18bdf5ee38eef7e2
ganesh <ganeshsadhashivam@outlook.com>
2023-04-30 12:56:17 +0530

    password verify

--- src/helper/validate.js
+++ src/helper/validate.js
@@ -10,6 +10,41 @@ export async function usernameValidate(values) {
   return errors;
 }
 
+/* validate password */
+
+export async function passwordValidate(values) {
+  const errors = passwordVerify({}, values);
+
+  return errors;
+}
+
+function passwordVerify(errors = [], values) {
+  /*
+    <li>Password must be 8-12 characters</li>
+    <li>At least one alphabet (uppercase or lowercase)</li>
+    <li>At least one number present in the password</li>
+    <li>At least one special character (-,.,@,$,!,%,+,=,<,>,#,?,&)</li>
+
+    */
+
+  const specialChars =
+    /^(?=.*[-\#\$\.\%\&\@\!\+\=\<\>\*])(?=.*[a-zA-Z])(?=.*\d).{8,12}$/;
+
+  if (!values.password) {
+    errors.password = toast.error("Password required...!");
+  } else if (values.username.includes(" ")) {
+    errors.password = toast.error("Wrong password...!");
+  } else if (values.password.length < 4) {
+    errors.password = toast.error(
+      "Password Must be more than 4 characters ...!"
+    );
+  } else if (!specialChars.test(values.password)) {
+    errors.password = toast.error("Password must have special Characters");
+  }
+
+  return errors;
+}
+
 /* validate username */
 function usernameVerify(error = {}, values) {
   if (!values.username) {
